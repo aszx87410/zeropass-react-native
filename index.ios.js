5214c77f9d4b4fca61936b6813815459feda812c
huli <huli@garena.com>
2016-11-04 22:12:07 +0800

    add ios icon

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
+  BackAndroid,
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator
 } from 'react-native';
+
+import MainScene from './scenes/MainScene';
 
 export default class zeropass extends Component {
   render() {
+    var defaultName = "MainScene";
+    var defaultComp = MainScene;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+      <Navigator
+        initialRoute={{
+          name: defaultName,
+          component: defaultComp
+        }}
+        configureScene={(route) => {
+          return Navigator.SceneConfigs.PushFromRight;
+        }}
+        renderScene={(route, navigator) => {
+          var Component = route.component;
+          return (
+            <View style={{flex: 1}}>
+              {route.name!=='MainScene' && route.name!=='UserMainScene' &&  (<Text style={{
+                position: 'absolute',
+                top: 20,
+                left: 10,
+                color: 'rgb(78, 186, 232)',
+                zIndex: 100,
+                fontSize: 22,
+              }} onPress={()=>{navigator.pop();}}>Back</Text>) }
+              <Component {...route.params} navigator={navigator} />
+            </View>
+          );
+        }}
+      />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('zeropass', () => zeropass);
